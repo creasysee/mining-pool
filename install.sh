932b09146743ec9561a4b224b009f6f189e6fdc2
Gennadiy Malyutin <creasysee@yandex.ru>
2023-08-31 20:26:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,37 @@
 #!/bin/sh
 
 # This is the Pool install script.
-echo "JAMPS Ravencoin Pool install script."
-echo "Please do NOT run as root, run as the pool user!"
+echo "######   Mining Pool install script."
+echo "######   Please do NOT run as root, run as the pool user!"
 
-echo "Installing... Please wait!"
+echo "######   Installing... Please wait!"
 
-sleep 5
+sleep 3
 
+echo "######   Cleanup the old installation..."
 sudo rm -rf /usr/lib/node_modules
 sudo rm -rf node_modules
 sudo apt remove --purge -y nodejs node
 sudo rm /etc/apt/sources.list.d/*
+
+echo "######   Installing requred libs..."
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt install -y apt-transport-https software-properties-common build-essential autoconf pkg-config make gcc g++ screen wget curl ntp fail2ban 
 
+echo "######   Adding repositories..."
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo add-apt-repository -y ppa:certbot/certbot
 
+echo "######   Installing required depends..."
 sudo apt update
 sudo apt install -y libdb4.8-dev libdb4.8++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev redis-server
 sudo apt install -y sudo git npm nodejs nginx python-certbot-nginx
 
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+#sudo systemctl enable fail2ban
+#sudo systemctl start fail2ban
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
 sudo systemctl enable ntp
@@ -34,20 +39,26 @@ sudo systemctl start ntp
 
 sudo rm -rf ~/.nvm
 sudo rm -rf ~/.npm
+
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+chmod +x ~/.nvm/nvm.sh
 source ~/.bashrc
+
 sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm install v8.17.0
 nvm use v8.17.0
 npm update -g
 
+npm install -g webpack@4.46.0
 npm install -g pm2@4.5.6
 npm install -g npm@latest-6
 
 npm install
+
 npm update
 npm audit fix
 npm install sha3
